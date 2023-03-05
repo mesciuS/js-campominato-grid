b871dd7aded749702e2e25dcc2aac23120e7c876
mesciuS <giovadev@gmail.com>
2023-03-05 19:01:24 +0100

    feat: html livelli

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.Bonus
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
@@ -16,21 +12,25 @@ let gridEl = document.getElementById("container");
 btnEl.addEventListener('click', function() {
     
     gridEl.innerHTML = '';
-
+    
+    // Ogni cella ha un numero progressivo, da 1 a 100.
     for(let i = 1; i <= 100; i++) {
         // creo e appendo un quadrato
+        // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
         let squareEl = document.createElement("div");
         squareEl.classList.add("square")
         gridEl.append(squareEl);
         squareEl.textContent = i;
-
+        
         // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.Bonus
-
+        
         squareEl.addEventListener('click', function() {
             squareEl.classList.toggle('select');
             console.log(i);
         })
-
+        
     }
-
+    
 });
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
